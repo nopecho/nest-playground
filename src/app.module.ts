3c7f05d027209a211b818e2c90d2a569487fbd73
nopecho <ch.jooon@gmail.com>
2023-02-05 19:13:08 +0900

    feat: imports nest config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {ConfigModule} from "@nestjs/config";
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'
+    })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}

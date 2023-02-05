6cc512a2e544ef4eace83226c933efa3290f78cc
nopecho <ch.jooon@gmail.com>
2023-02-05 19:16:25 +0900

    feat: add joi & config validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import {Module} from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
+import Joi from "joi";
 
 @Module({
     imports: [ConfigModule.forRoot({
         isGlobal: true,
-        envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev'
+        envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
+        validationSchema : Joi.object({
+            NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        })
     })],
     controllers: [AppController],
     providers: [AppService],
